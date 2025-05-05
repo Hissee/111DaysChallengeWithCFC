84a2675ff62d5112d1062afbd7f5da4d78095af6
Hissee <maharjanhissee@gmail.com>
2025-05-05 19:43:51 +0545

    Day 21

---
+++ Day 21 DOM Manipulation/script.js
@@ -0,0 +1,10 @@
+let x = document.getElementById("heading1")
+
+console.dir(x);
+
+let y = document.getElementsByClassName("heading")
+
+console.dir(y);
+
+let z = document.getElementsByTagName("h1");
+console.dir(z);
